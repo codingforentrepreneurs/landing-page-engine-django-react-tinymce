f01963a588a2755f0adade8086e52b02a715aedd
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-30 16:29:53 -0700

    13 - WYSIWYG React & TinyMCE

--- frontend/editor/tiny.jsx
+++ frontend/editor/tiny.jsx
@@ -1,36 +1,47 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function TinyMCE() {
+  const [content, setContent] = useState('')
   const editorRef = useRef(null);
   const apiKey = 'd7dk00vzzntbn3ltvykf7jd9j0017pxs3u3lu1tkiwmlyzec' // Tiny.cloud dashboard
-  const log = () => {
+  const handleChange = useCallback(() => {
     if (editorRef.current) {
-      console.log(editorRef.current.getContent());
+      setContent(editorRef.current.getContent())
     }
-  };
+  }, [editorRef])
+
+  const handleOnInit = (evt, editor) => {
+    editorRef.current = editor
+    handleChange()
+  }
   return (
-    <>
-      <Editor
-        apiKey={apiKey}
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button onClick={log}>Log editor content</button>
-    </>
+    <div className="side-by-side">
+    
+      <div>
+        <Editor
+          apiKey={apiKey}
+          onEditorChange={handleChange}
+          onInit={handleOnInit}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            ],
+            toolbar: 'undo redo | blocks | ' +
+              'bold italic forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <button onClick={handleChange}>Save</button>
+    </div>
+    <div>{content ? <div dangerouslySetInnerHTML={{__html:content}}></div> : null}</div>
+  </div>
   );
 }
