58162e629b527d5e94de81e4ba932a3146c99201
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-29 17:06:23 -0700

    8 - Prepare Django Static Files with Whitenoise

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# nginx, s3 -> django-storages, whitenoise
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
+STATIC_ROOT = BASE_DIR / 'my-cdn'
+if DEBUG:
+    STATIC_ROOT = BASE_DIR.parent / 'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
